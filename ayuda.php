f8220853cf7a7581633811a371c041267adb86ef
PaulHeredia <spheredia1@espe.edu.ec>
2020-08-12 13:21:31 -0500

    Ayuda con Tablas

--- ayuda.php
+++ ayuda.php
@@ -8,5 +8,12 @@
 <body>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia labore quos aliquid vel asperiores corporis, unde eveniet cupiditate vitae culpa suscipit excepturi neque laudantium, nostrum mollitia deleniti at, inventore modi!</p>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit dolores nam dolorem ut aut dolore, laborum iure exercitationem molestias? Vitae, animi fugiat. Quod eius quis quibusdam! Officiis architecto quae nulla!</p>
+    <table>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
 </body>
 </html>
