eff85f1783e31627c7c7e36b476527e08e3eba52
PaulHeredia <spheredia1@espe.edu.ec>
2020-08-12 12:57:43 -0500

    Ayuda y Portada

--- ayuda.php
+++ ayuda.php
@@ -6,6 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia labore quos aliquid vel asperiores corporis, unde eveniet cupiditate vitae culpa suscipit excepturi neque laudantium, nostrum mollitia deleniti at, inventore modi!</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit dolores nam dolorem ut aut dolore, laborum iure exercitationem molestias? Vitae, animi fugiat. Quod eius quis quibusdam! Officiis architecto quae nulla!</p>
 </body>
 </html>
