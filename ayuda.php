05d8c6df34747d1eb8ca4d31708baae92b107ac6
PaulHeredia <spheredia1@espe.edu.ec>
2020-08-12 13:20:17 -0500

    Cambios Ayuda - Interface

--- ayuda.php
+++ ayuda.php
@@ -7,6 +7,8 @@
 </head>
 <body>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia labore quos aliquid vel asperiores corporis, unde eveniet cupiditate vitae culpa suscipit excepturi neque laudantium, nostrum mollitia deleniti at, inventore modi!</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit dolores nam dolorem ut aut dolore, laborum iure exercitationem molestias? Vitae, animi fugiat. Quod eius quis quibusdam! Officiis architecto quae nulla!</p>
+    <p>Texto de Ayuda</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis amet ratione quaerat voluptatem voluptates numquam, tempore doloribus quae impedit fuga modi a eos aliquid nesciunt quod similique atque omnis illum.</p>
+    
 </body>
 </html>
